5a259f6fe05cb7200e98bd286933a80e0e5db8a0
nabilk11 <khan.nabiln11@gmail.com>
2022-02-23 11:22:28 -0500

    moved onto client side react app - setup initial folder structure with navbar component, home page, and material-ui icons. also added Roboto font family

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import './navbar.css';
+import { Chat, Notifications, Person, Search } from '@material-ui/icons'
+
+export default function Navbar() {
+  return (
+    <div className='navbarContainer'>
+        <div className="navbarLeft">
+            <span className="logo">nabBook</span>
+        </div>
+        <div className="navbarCenter">
+            <div className="searchbar">
+            <Search />
+            <input placeholder='Search...' className="searchInput" />
+            </div>
+        </div>
+        <div className="navbarRight">
+            <div className="navbarLinks">
+                <span className="navbarLink">Home</span>
+                <span className="navbarLink">Timeline</span>
+            </div>
+            <div className="navbarIcons">
+                <div className="navbarIconItem">
+                  <Person />
+                  <span className="navbarIconBadge">1</span> 
+                </div>
+                <div className="navbarIconItem">
+                  <Chat />
+                  <span className="navbarIconBadge">2</span> 
+                </div>
+                <div className="navbarIconItem">
+                  <Notifications />
+                  <span className="navbarIconBadge">1</span> 
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
