5d445708667491ae9d97d0b54b164ca50166e82f
nabilk11 <khan.nabiln11@gmail.com>
2022-02-23 07:19:26 -0500

    created delete post route, and put route for adding/removing likes from posts - both tested in postman

--- NODE-API/routes/posts.js
+++ NODE-API/routes/posts.js
@@ -36,8 +36,45 @@ router.put("/:id", async (req, res) => {
 })
 
 //DELETE POST
+router.delete("/:id", async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id);
+    if (post.userId === req.body.userId) {
+        await post.deleteOne();
+        res.status(200).json("Your post has been deleted!")
 
-//LIKE A POST
+    } else {
+        res.status(403).json("You can only delete your own posts!")
+    }
+        
+    } catch (err) {
+        res.status(500).json(err)
+        
+    }
+    
+})
+//LIKE or UNLIKE A POST
+
+router.put("/:id/like", async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id)
+        if (!post.likes.includes(req.body.userId)) {
+            await post.updateOne({
+                $push: { likes: req.body.userId}
+            })
+            res.status(200).json("The post was liked!")
+        } else {
+            await post.updateOne({
+                $pull: { likes: req.body.userId }
+            })
+            res.status(200).json("The post was unliked!")
+        }
+        
+    } catch (err) {
+        res.status(500).json(err)
+        
+    }
+})
 
 //GET A POST
 
