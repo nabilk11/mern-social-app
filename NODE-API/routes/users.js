d38636fcb6aa762be6752274df9c62be6464a17d
nabilk11 <khan.nabiln11@gmail.com>
2022-02-23 02:59:48 -0500

    created base for users and auth routes, with middleware and routers

---
+++ NODE-API/routes/users.js
@@ -0,0 +1,13 @@
+const router = require('express').Router();
+
+
+router.get("/", (req, res) => {
+    res.send("users route!")
+})
+
+
+
+
+
+
+module.exports = router;
--- NODE-API/server.js
+++ NODE-API/server.js
@@ -9,12 +9,25 @@ const helmet = require('helmet')
 const morgan = require('morgan')
 const PORT = 8000;
 
+// INTERNAL ROUTES
+const userRoute = require('./routes/users');
+const authRoute = require('./routes/auth');
+
 // MONGO DB CONNECTION
 mongoose.connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
 }).then(()=> {
     console.log('MongoDB is Connected')
 })
+
+// MIDDLEWARE
+app.use(express.json())
+app.use(helmet())
+app.use(morgan('common'))
+
+// ROUTERS
+app.use('/api/users', userRoute)
+app.use('/api/auth', authRoute)
 
 
 // LISTENER
