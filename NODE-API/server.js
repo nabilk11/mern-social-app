e40b43ceb9101a25dedd33201a2006c621d1cbfe
nabilk11 <khan.nabiln11@gmail.com>
2022-02-23 02:43:34 -0500

    connected to mongodb

--- NODE-API/server.js
+++ NODE-API/server.js
@@ -1,6 +1,20 @@
+// DOTENV
+const dotenv = require('dotenv')
+dotenv.config();
+// EXTERNALS
 const express = require('express')
 const app = express();
+const mongoose = require('mongoose')
+const helmet = require('helmet')
+const morgan = require('morgan')
 const PORT = 8000;
+
+// MONGO DB CONNECTION
+mongoose.connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+}).then(()=> {
+    console.log('MongoDB is Connected')
+})
 
 
 // LISTENER
