f72c9802c9fa5818c3801253e408905eed63fb26
nabilk11 <khan.nabiln11@gmail.com>
2022-02-23 03:30:12 -0500

    created register user route in auth.js

--- NODE-API/routes/auth.js
+++ NODE-API/routes/auth.js
@@ -1,8 +1,21 @@
 const router = require('express').Router();
+const User = require('../models/User')
 
-
-router.get("/", (req, res) => {
-    res.send("users route!")
+// REGISTER USER
+router.post("/register", async (req, res) => {
+    const newUser = await new User({
+        username: req.body.username,
+        email: req.body.email,
+        password: req.body.password,
+    })
+    try {
+        const user = await newUser.save();
+        res.status(200).json(user)
+    } catch (err) {
+        console.log(`Error registering user: ${err}`)
+        
+    }
+   
 })
 
 
