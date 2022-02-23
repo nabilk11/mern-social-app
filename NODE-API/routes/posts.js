c3a5709cb8382a386790c87adbb90fee3f3355de
nabilk11 <khan.nabiln11@gmail.com>
2022-02-23 06:55:13 -0500

    created update post route in posts.js - tested in postman

--- NODE-API/routes/posts.js
+++ NODE-API/routes/posts.js
@@ -15,6 +15,25 @@ router.post("/", async (req, res) => {
 })
 
 //UPDATE POST
+router.put("/:id", async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id);
+    if (post.userId === req.body.userId) {
+        await post.updateOne({
+            $set: req.body
+        });
+        res.status(200).json("Your post has been updated!")
+
+    } else {
+        res.status(403).json("You can only update your own posts!")
+    }
+        
+    } catch (err) {
+        res.status(500).json(err)
+        
+    }
+    
+})
 
 //DELETE POST
 
