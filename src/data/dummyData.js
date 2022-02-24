4cd015757cfeb84746e8f670c3f43207c6af5c86
nabilk11 <khan.nabiln11@gmail.com>
2022-02-24 11:56:51 -0500

    created data/dummyData.js - holding dummy data for 8 users and 8 posts

---
+++ src/data/dummyData.js
@@ -0,0 +1,112 @@
+export const Users = [
+    {
+      id:1,
+      profilePicture: "assets/lebron_pf.png",
+      username: "Lebron James",
+    },
+    {
+      id:2,
+      profilePicture: "assets/bellah_pf.jpeg",
+      username: "Bella Hadid",
+    },
+    {
+      id:3,
+      profilePicture: "assets/drake_pf.jpeg",
+      username: "Aubrey Graham",
+    },
+    {
+      id:4,
+      profilePicture: "assets/samhar_pf.jpg",
+      username: "Sam Harris",
+    },
+    {
+      id:5,
+      profilePicture: "assets/iggyaz_pf.jpeg",
+      username: "Iggy Azalea",
+    },
+    {
+      id:6,
+      profilePicture: "assets/pikachu_pf.jpeg",
+      username: "Pikachu",
+    },
+    {
+      id:7,
+      profilePicture: "assets/rihanna_pf.jpeg",
+      username: "Robin Fenty",
+    },
+    {
+      id:8,
+      profilePicture: "assets/tonstark_pf.webp",
+      username: "Tony Stark",
+    },
+  ];
+  
+  export const Posts = [
+    {
+      id: 1,
+      desc: "Love For All, Hatred For None.",
+      photo: "assets/post/1.jpeg",
+      date: "5 mins ago",
+      userId: 1,
+      like: 32,
+      comment: 9,
+    },
+    {
+      id: 2,
+      photo: "assets/post/2.jpeg",
+      date: "15 mins ago",
+      userId: 2,
+      like: 2,
+      comment: 1,
+    },
+    {
+      id: 3,
+      desc: "Every moment is a fresh beginning.",
+      photo: "assets/post/3.jpeg",
+      date: "1 hour ago",
+      userId: 3,
+      like: 61,
+      comment: 2,
+    },
+    {
+      id: 4,
+      photo: "assets/post/4.jpeg",
+      date: "4 hours ago",
+      userId: 4,
+      like: 7,
+      comment: 3,
+    },
+    {
+      id: 5,
+      photo: "assets/post/5.jpeg",
+      date: "5 hours ago",
+      userId: 5,
+      like: 23,
+      comment: 5,
+    },
+    {
+      id: 6,
+      photo: "assets/post/6.jpeg",
+      date: "1 day ago",
+      userId: 6,
+      like: 44,
+      comment: 6,
+    },
+    {
+      id: 7,
+      desc: "Never regret anything that made you smile.",
+      photo: "assets/post/7.jpeg",
+      date: "2 days ago",
+      userId: 7,
+      like: 52,
+      comment: 3,
+    },
+    {
+      id: 8,
+      photo: "assets/post/8.jpeg",
+      date: "3 days ago",
+      userId: 8,
+      like: 15,
+      comment: 1,
+    },
+  ];
