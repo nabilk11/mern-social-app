293dce7f9476f34a06c84b289f34fe2c5102bd75
nabilk11 <khan.nabiln11@gmail.com>
2022-02-23 20:18:20 -0500

    created feed, and left/right bar components, and imported onto home page, with initial display styling

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,21 @@
 import React from 'react';
-import Navbar from '../../components/Navbar/Navbar';
+import Navbar from '../../components/navbar/Navbar';
+import Leftbar from '../../components/leftbar/Leftbar';
+import Feed from '../../components/feed/Feed';
+import Rightbar from '../../components/rightbar/Rightbar';
+import './home.css';
 
 export default function Home() {
   return (
     <div>
         <Navbar />
-        this is the Homepage 
+        <div className="homeContainer">
+        <Leftbar />
+        <Feed />
+        <Rightbar />
+
+        </div>
+        
     </div>
   )
 }
