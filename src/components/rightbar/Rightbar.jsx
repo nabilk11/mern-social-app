9223534a0e76c018540f2bc64a4042bb9dda366f
nabilk11 <khan.nabiln11@gmail.com>
2022-02-24 03:18:06 -0500

    completed rightbar component ui and styling - social media app layout completed

--- src/components/rightbar/Rightbar.jsx
+++ src/components/rightbar/Rightbar.jsx
@@ -4,7 +4,39 @@ import './rightbar.css';
 export default function Rightbar() {
   return (
     <div className='rightbar'>
-        rightbar
+      <div className="rightbarWrapper">
+        <div className="birthdayContainer">
+          <img className="birthdayImg" src="/assets/ui/bday.png" alt="" />
+          <span className="birthdayText">
+            <b>Afsana Miji</b> and <b> 2 other friends</b> have a birthday today!
+          </span>
         </div>
+        <img className="rightbarAd"src="/assets/ui/delta_ad.png" alt="" />
+      <h4 className="rightbarTitle">Online Friends</h4>
+      <ul className="rightbarFriendList">
+        <li className="rightbarFriend">
+          <div className="rightbarProfileImgContainer">
+            <img src="/assets/lebron_pf.png" alt="" className="rightbarProfileImg" />
+            <span className="rightbarOnline"></span>
+          </div>
+          <span className="rightbarUsername">Lebron James</span>
+        </li>
+        <li className="rightbarFriend">
+          <div className="rightbarProfileImgContainer">
+            <img src="/assets/lebron_pf.png" alt="" className="rightbarProfileImg" />
+            <span className="rightbarOnline"></span>
+          </div>
+          <span className="rightbarUsername">Lebron James</span>
+        </li>
+        <li className="rightbarFriend">
+          <div className="rightbarProfileImgContainer">
+            <img src="/assets/lebron_pf.png" alt="" className="rightbarProfileImg" />
+            <span className="rightbarOnline"></span>
+          </div>
+          <span className="rightbarUsername">Lebron James</span>
+        </li>
+      </ul>
+      </div>        
+  </div>
   )
 }
