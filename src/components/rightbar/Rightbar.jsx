88afa92b71ce6ce6f8c51c16270e36e467e0a018
nabilk11 <khan.nabiln11@gmail.com>
2022-02-24 21:23:22 -0500

    split rightbar into two separate components for home and profile page, completed profile page, with styling

--- src/components/rightbar/Rightbar.jsx
+++ src/components/rightbar/Rightbar.jsx
@@ -3,11 +3,12 @@ import './rightbar.css';
 import { Users } from '../../data/dummy_data';
 import OnlineFriends from '../onlineFriends/OnlineFriends';
 
-export default function Rightbar() {
-  return (
-    <div className='rightbar'>
-      <div className="rightbarWrapper">
-        <div className="birthdayContainer">
+export default function Rightbar({ profile }) {
+
+  const HomeRightbar = () => {
+    return (
+      <>
+      <div className="birthdayContainer">
           <img className="birthdayImg" src="/assets/ui/bday.png" alt="" />
           <span className="birthdayText">
             <b>Afsana Miji</b> and <b> 2 other friends</b> have a birthday today!
@@ -20,6 +21,53 @@ export default function Rightbar() {
           <OnlineFriends user={u} key={u.id}/>
         ))}
       </ul>
+      </>
+    )
+  }
+  const ProfileRightbar = () => {
+    return (
+      <>
+      <h4 className='rightbarTitle'>User Information</h4>
+      <div className="rightbarInfo">
+        <div className="rightbarInfoItem">
+          <span className="rightbarInfoKey">City:</span>
+          <span className="rightbarInfoValue">New York City</span>
+        </div>
+        <div className="rightbarInfoItem">
+          <span className="rightbarInfoKey">From:</span>
+          <span className="rightbarInfoValue">New York City</span>
+        </div>
+        <div className="rightbarInfoItem">
+          <span className="rightbarInfoKey">Relationship Status:</span>
+          <span className="rightbarInfoValue">In a Relationship</span>
+        </div>
+      </div>
+      <h4 className="rightbarTitle">User Friends</h4>
+      <div className="rightbarFollowings">
+      <div className="rightbarFollowing">
+        <img src="/assets/bellah_pf.jpeg" alt="" className="rightbarFollowingImg" />
+        <span className="rightbarFollowingName">Bella Hadid</span>
+      </div>
+      <div className="rightbarFollowing">
+        <img src="/assets/lebron_pf.png" alt="" className="rightbarFollowingImg" />
+        <span className="rightbarFollowingName">Lebron James</span>
+      </div>
+      <div className="rightbarFollowing">
+        <img src="/assets/samhar_pf.jpg" alt="" className="rightbarFollowingImg" />
+        <span className="rightbarFollowingName">Sam Harris</span>
+      </div>
+      <div className="rightbarFollowing">
+        <img src="/assets/tonstark_pf.webp" alt="" className="rightbarFollowingImg" />
+        <span className="rightbarFollowingName">Tony Stark</span>
+      </div>
+      </div>
+      </>
+    )
+  }
+  return (
+    <div className='rightbar'>
+      <div className="rightbarWrapper">
+        <ProfileRightbar />
       </div>        
   </div>
   )
