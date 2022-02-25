e778086a45b722b80396db7768a0ca5786579fcc
nabilk11 <khan.nabiln11@gmail.com>
2022-02-24 22:09:07 -0500

    created login component page UI with styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import Home from "./pages/Home/Home";
+import Login from "./pages/login/Login";
 import Profile from "./pages/profile/Profile";
 
 function App() {
   return (
-    <Profile />
+    <Login />
   );
 }
 
